9917a2a802549932244771eeff34fe131e3dfa6b
Vuong Khac Duy <vuongkhacduy@gmail.com>
2025-09-04 14:41:40 +0700

    update code

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "../assets/css/style.css";
+import "../assets/css/layout-header-footer-menu.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
